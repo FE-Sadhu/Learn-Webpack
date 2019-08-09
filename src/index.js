15fe3868df96487470b62a75ebe8d9af95cf1e61
sadhu <1714222385@qq.com>
2019-08-09 15:54:10 +0800

    HMR for css's example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hello world! sadhu111');
+import './index.css'
+
+var btn = document.createElement('button');
+btn.innerHTML = '新增';
+
+btn.onclick = function() {
+  var div = document.createElement('div');
+  div.innerHTML = 'item';
+  document.body.append(div);
+}
+
+document.body.append(btn);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const Webpack = require('webpack');
 
 module.exports = {
   mode: 'development', // 开发者模式默认开启了 sourceMap
@@ -10,7 +11,9 @@ module.exports = {
   },
   devServer: { // 提升开发效率，起个服务器，当修改源代码后，自动打包并且响应在页面上。
     contentBase: './dist', // 以 dist 目录下文件起一个本地服务器
-    open: true // 配置后，当我们执行打包完成后，会自动打开浏览器，访问该本地服务器。
+    open: true, // 配置后，当我们执行打包完成后，会自动打开浏览器，访问该本地服务器。
+    hot: true, // 开启热模块更新(HMR hot module replacement)
+    hotOnly: true // 加上这个配置是为了当热模块更新失败，不让webpack做任何操作，不然webpack会刷新页面。
   },
 	module: {
 		rules: [{
@@ -41,11 +44,18 @@ module.exports = {
 				'sass-loader',
 				'postcss-loader'
 			]
+		}, {
+			test: /\.css$/,
+			use: [
+				'style-loader', 
+				'css-loader'
+			]
 		}]
 	},
 	plugins: [new HtmlWebpackPlugin({
 		template: 'src/index.html'
-	}), new CleanWebpackPlugin(['dist'])],
+  }), new CleanWebpackPlugin(['dist']),
+  new Webpack.HotModuleReplacementPlugin()],
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
